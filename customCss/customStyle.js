2e68b5ab6bc21ee99f74ecb44d434e4911432bb2
michelebuccarello <mbuccarello@sowre.com>
2017-10-05 09:48:29 +0200

    clean up folders

--- customCss/customStyle.js
+++
@@ -1,7 +0,0 @@
-if(typeof(dojo) != "undefined") {
-    dojo.place(
-        "<link rel=\"stylesheet\" type=\"text/css\" href=\"/files/muse-static/customCss/style.css\"></link>",
-        dojo.doc.head,
-        "last"
-    );
-}

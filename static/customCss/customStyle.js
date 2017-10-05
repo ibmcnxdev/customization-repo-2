8b0e020d7232feee2a92cc657209330adeadb7c9
michelebuccarello <mbuccarello@sowre.com>
2017-10-05 08:56:49 +0200

    css commit

---
+++ static/customCss/customStyle.js
@@ -0,0 +1,7 @@
+if(typeof(dojo) != "undefined") {
+    dojo.place(
+        "<link rel=\"stylesheet\" type=\"text/css\" href=\"/files/muse-static/customCss/style.css\"></link>",
+        dojo.doc.head,
+        "last"
+    );
+}

36d79aef911519e99215fd3f25504d6d4e649dbb
michelebuccarello <mbuccarello@sowre.com>
2017-10-05 10:22:20 +0200

    changed url for css

--- static/customCss/customStyle.js
+++ static/customCss/customStyle.js
@@ -1,6 +1,6 @@
 if(typeof(dojo) != "undefined") {
     dojo.place(
-        "<link rel=\"stylesheet\" type=\"text/css\" href=\"/files/muse-static/customCss/style.css\"></link>",
+        "<link rel=\"stylesheet\" type=\"text/css\" href=\"/files/muse-static/customCss/style.css?repoName=customization-repo-2\"></link>",
         dojo.doc.head,
         "last"
     );
